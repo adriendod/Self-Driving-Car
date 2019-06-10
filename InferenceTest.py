4b8b1d8208b722810c396136423e25ca948b2400
Adrien Dodinet <adrien.dod@gmail.com>
2019-06-10 19:41:59 +0200

    inference test

--- InferenceTest.py
+++ InferenceTest.py
@@ -23,10 +23,16 @@ def capture_yuv():
 def turn_prediction(img):
     return model.predict(img)
 
-start = time.time()
-img = capture_yuv()
-prediction = turn_prediction(img)
-end = time.time()
-capture_duration = end - start
-print("Turn took " + str(capture_duration) + " seconds to be predicted.")
-print(prediction)
+for i in range(5):
+    start = time.time()
+    img = capture_yuv()
+    end = time.time()
+    capture_duration = end - start
+    print("Image took " + str(capture_duration) + " seconds to be processed.")
+
+    start = time.time()
+    prediction = turn_prediction(img)
+    end = time.time()
+    capture_duration = end - start
+    print("Turn took " + str(capture_duration) + " seconds to be predicted.")
+    print(prediction)
