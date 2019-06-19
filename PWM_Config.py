238e617dcdb8ad535c7357686c15cf82b9a07b2f
DODINET Adrien <adodinet@largus.fr>
2019-06-19 14:16:37 +0200

    FeatherWing MotorKit

--- PWM_Config.py
+++ PWM_Config.py
@@ -1,53 +1,45 @@
 import Jetson.GPIO as GPIO
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+kit = MotorKit()
 from time import sleep
 
+
+mh = Adafruit_MotorHAT(addr=0x60)
+
+direction = mh.getMotor(1)
+acceleration = mh.getMotor(2)
+
+# set the speed to start, from 0 (off) to 255 (max speed)
+direction.setSpeed(150)
+acceleration.setSpeed(150)
+
+direction.run(Adafruit_MotorHAT.FORWARD)
+# turn on motor
+direction.run(Adafruit_MotorHAT.RELEASE)
+
 #///////////////// Define Motor Driver GPIO Pins /////////////////
-
-GPIO.setmode(GPIO.BOARD)
-
-PWM_FORWARD_PIN = 27	
-PWM_REVERSE_PIN = 22	
-
-PWM_RIGHT_PIN = 4	
-PWM_LEFT_PIN = 17	
-
-# Initialise objects for H-Bridge PWM pins
-# Set initial duty cycle to 0 and frequency to 1000
-forward = GPIO.PWM(PWM_FORWARD_PIN, 1000)
-reverse = GPIO.PWM(PWM_REVERSE_PIN, 1000)
- 
-right = GPIO.PWM(PWM_RIGHT_PIN, 1000)
-left = GPIO.PWM(PWM_LEFT_PIN, 1000)
 
 speed = 0.3
 
 def allStop():
-	forward.value = 0
-	reverse.value = 0
-	right.value = 0
-	left.value = 0
+	direction.run(Adafruit_MotorHAT.RELEASE)
+    acceleration.run(Adafruit_MotorHAT.RELEASE)
 
 def forwardDrive():
-	forward.value = speed
-	reverse.value = 0
+	acceleration.run(Adafruit_MotorHAT.FORWARD)
 
 def reverseDrive():
-	forward.value = 0
-	reverse.value = speed
+	acceleration.run(Adafruit_MotorHAT.BACKWARD)
 
 def stopDrive():
-	forward.value = 0
-	reverse.value = 0	
+	acceleration.run(Adafruit_MotorHAT.RELEASE)	
 
 def turnRight():
-	right.value = 1.0
-	left.value = 0
+	direction.run(Adafruit_MotorHAT.FORWARD)
 
 def turnLeft():
-	right.value = 0
-	left.value = 1.0
+	direction.run(Adafruit_MotorHAT.BACKWARD)
 
 def goStraight():
-	right.value = 0
-	left.value = 0
+	direction.run(Adafruit_MotorHAT.RELEASE)
 
---
+++ PWM_Config_MotorKit.py
@@ -0,0 +1,32 @@
+import Jetson.GPIO as GPIO
+from adafruit_motorkit import motorkit
+kit = MotorKit()
+
+direction = mh.getMotor(1)
+acceleration = mh.getMotor(2)
+
+
+speed = 0.3
+
+def allStop():
+	kit.motor1.throttle = None
+    kit.motor2.throttle = None
+
+def forwardDrive():
+	kit.motor1.throttle = speed
+
+def reverseDrive():
+	kit.motor1.throttle = -speed
+
+def stopDrive():
+	kit.motor1.throttle = None
+
+def turnRight():
+	kit.motor2.throttle = 1
+
+def turnLeft():
+	kit.motor2.throttle = -1
+
+def goStraight():
+	kit.motor2.throttle = None
+
