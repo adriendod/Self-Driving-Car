1a2750916fa9a06ae841fde7cb9e100b23bc0445
Adrien Dodinet <adrien.dod@gmail.com>
2019-07-28 22:21:30 +0200

    Camera class

--- drive_test.py
+++ drive_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 import cv2
 
 #camera
-cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)NV12, framerate=(fraction)24/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+camera = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)NV12, framerate=(fraction)24/1 ! nvvidconv flip-method=2 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 
 
 
